9103b42d9d8870796084d4496a75a79dd5e40c01
Igor Sukharev <imixaly4@gmail.com>
2021-03-27 09:22:39 +0200

    clean up

--- js/list.js
+++ js/list.js
@@ -1,8 +1,6 @@
 import {memMap} from '@perform/core/helpers';
 
 import {section, input, label, ul} from './html';
-import {item1} from './item1';
-import {item2} from './item2';
 import {item} from './item';
 
 export const list = ({
@@ -19,26 +17,6 @@ export const list = ({
       onchange: ({target: {checked}}) => updateAll({completed: checked})?.(),
     }),
     label({for: 'toggle-all'}),
-
-    // () => ul({class: 'todo-list'}, ...getRouteItems().map(({id, title, completed}) => item1({
-    // ul({class: 'todo-list'}, () => getRouteItems().map(({id, title, completed}) => item1({
-    //   id,
-    //   title,
-    //   completed,
-    //   updateTitle,
-    //   updateCompleted,
-    //   remove,
-    // }))),
-
-    // ul({class: 'todo-list'}, memMap(getRouteItems, getItem => item2({
-    //   id: getItem().id,
-    //   title: getItem().title,
-    //   getCompleted: () => getItem().completed,
-    //   updateTitle,
-    //   updateCompleted,
-    //   remove,
-    // }))),
-
     ul({class: 'todo-list'}, memMap(getRouteItems, getItem => item({
       getItem,
       update,
